d52782b1f7f1b7a88b669f41aa9e03278ad7176d
Matt Dudzik <mdudzik92@gmail.com>
2021-09-18 20:51:19 -0500

    Context created with the GlobalState, Provider component created to allow other components to access the global state, and AppReducer to specify state changes

---
+++ src/context/AppReducer.js
@@ -0,0 +1,9 @@
+export default (state, action) => {
+  switch(action.type) {
+    default: 
+      return state;
+  }
+}
+
+
+// A reducer is how you specify the application's state changes in response to certain actions to the context
---
+++ src/context/GlobalState.js
@@ -0,0 +1,32 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+// Initial state
+// Any global state would go in this object.
+// However, all we need is our transactions because as long as we have access to the transactions in certain components we can do our calculations there. Like for the balance, etc. 
+// We just need to be able to pass this data down, and then we'll have actions like deleting and adding transactions from this piece of state
+const initialState = {
+  transactions: [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 }
+  ]
+}
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component - this allows other components to have access to our global state. Wraps all the components.
+export const GlobalProvider = ({ children }) => {
+  // Whenever we need to call a reducer action we need to use dispatch
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+
+  // What the provider does is it provides our state, and provides any actions to the components that it's wrapped around
+  return (<GlobalContext.Provider value={{
+    transactions: state.transactions
+  }}>
+    {children}
+  </GlobalContext.Provider>)
+}
