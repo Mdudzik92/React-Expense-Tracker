8febcfb24a698e52deff027331501d6c6b815418
Matt Dudzik <mdudzik92@gmail.com>
2021-09-18 21:17:30 -0500

    Updating plus or minus signs depending on expenses and additions, as well as changing border color of transactions

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,19 @@
-import React from 'react'
+// We can pull out anything from the global context through using the useContext hook
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+// Pulling global state in
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {/* Mapping through the transactions array in the global state, outputting each transaction */}
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+        
       </ul>
     </>
   )
