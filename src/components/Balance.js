2e709f21ba185876c0861467bbe841a8e015565e
Matt Dudzik <mdudzik92@gmail.com>
2021-09-18 21:36:47 -0500

    Calculating income and expenses and rendering them to the DOM

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  // Mapping through and getting all the amounts into an array 
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  // To get the total we're using reduce to add them all together and then using toFixed to get two decimal places 
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
 	return (
   <>
     <h4>Your Balance</h4>
-    <h1>$0.00</h1>
+    <h1>${total}</h1>
   </>
   )
 };
